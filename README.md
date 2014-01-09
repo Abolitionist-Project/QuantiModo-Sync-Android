5fb578d04e7915160d528feac30e4ecce6207544
Quint Stoffers <quipeace@gmail.com>
2014-01-09 17:41:17 +0100

    Clean repo

--- README.md
+++
@@ -1,2 +0,0 @@
-QuantiModo-Sync
-===============
--- etl/Converter.java
+++
@@ -1,9 +0,0 @@
-package com.quantimodo.etl;
-
-import com.quantimodo.sdk.model.QuantimodoMeasurement;
-
-public interface Converter
-{
-	QuantimodoMeasurement[] convert(DatabaseView databaseView);
-}
-
