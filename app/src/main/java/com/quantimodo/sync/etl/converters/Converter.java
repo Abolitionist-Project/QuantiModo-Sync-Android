2ed0ea2446e119de3da37ac7d7f6cdfafe12c02f
Quint Stoffers <quipeace@gmail.com>
2014-08-04 15:54:26 +0200

    Added getAppInfo method to Converter interface

--- app/src/main/java/com/quantimodo/sync/etl/converters/Converter.java
+++ app/src/main/java/com/quantimodo/sync/etl/converters/Converter.java
@@ -2,11 +2,14 @@ package com.quantimodo.sync.etl.converters;
 
 import com.quantimodo.android.sdk.model.MeasurementSet;
 import com.quantimodo.sync.etl.DatabaseView;
+import com.quantimodo.sync.model.SyncableAppInfo;
 
 import java.util.ArrayList;
 
 public interface Converter
 {
 	ArrayList<MeasurementSet> convert(DatabaseView databaseView);
+
+    SyncableAppInfo getAppInfo();
 }
 
